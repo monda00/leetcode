5545e7db9ccf175fcb49ebe933b59bca3ffcd864
monda00 <monda0524@gmail.com>
2025-06-10 21:48:06 +0900

    🚀 prob: roman to integer (java)

---
+++ java/problems/p0013_roman_to_integer/SolutionTest.java
@@ -0,0 +1,14 @@
+package problems.p0013_roman_to_integer;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolutionTest {
+    @Test
+    void testSolution() {
+        Solution solution = new Solution();
+        assertEquals(3, solution.romanToInt("III"));
+        assertEquals(58, solution.romanToInt("LVIII"));
+        assertEquals(1994, solution.romanToInt("MCMXCIV"));
+    }
+}
