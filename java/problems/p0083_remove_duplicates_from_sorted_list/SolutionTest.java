b80796e4c20a41450dcb7c63f01f9dae21fb047c
monda00 <monda0524@gmail.com>
2025-08-06 20:59:26 +0900

    🚀 prob: remove duplicates from sorted list

---
+++ java/problems/p0083_remove_duplicates_from_sorted_list/SolutionTest.java
@@ -0,0 +1,10 @@
+package problems.p0083_remove_duplicates_from_sorted_list;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolutionTest {
+    @Test
+    void testSolution() {
+    }
+}
